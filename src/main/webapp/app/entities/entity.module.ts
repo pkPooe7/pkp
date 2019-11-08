b3c8a5e01f2d6bc90f65d3f6c03735a5b540b473
Poloko <polokopooe@gmail.com>
2019-11-08 10:50:59 +0200

    Got the correct way of displaying the entities and the relationships

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,26 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'alien',
+        loadChildren: () => import('./alien/alien.module').then(m => m.ReBaseAlienModule)
+      },
+      {
+        path: 'world',
+        loadChildren: () => import('./world/world.module').then(m => m.ReBaseWorldModule)
+      },
+      {
+        path: 'solar-system',
+        loadChildren: () => import('./solar-system/solar-system.module').then(m => m.ReBaseSolarSystemModule)
+      },
+      {
+        path: 'classification',
+        loadChildren: () => import('./classification/classification.module').then(m => m.ReBaseClassificationModule)
+      },
+      {
+        path: 'technology',
+        loadChildren: () => import('./technology/technology.module').then(m => m.ReBaseTechnologyModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
